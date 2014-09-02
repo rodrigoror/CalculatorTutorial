b25ab645da27da2a3ae1673bd94f1fd948019876
Rodrigo Ramos <rodrigoror@gmail.com>
2014-09-02 15:24:48 -0300

    Tutorial de Xcode
    Calculadora para IOS usando Xcode5 - Commit Inicial

--- CalculatorTutorial/brViewController.h
+++ CalculatorTutorial/brViewController.h
@@ -8,6 +8,29 @@
 
 #import <UIKit/UIKit.h>
 
-@interface brViewController : UIViewController
+int Method;
+int SelectNumber;
+float RunningTotal;
 
+@interface brViewController : UIViewController
+{
+    IBOutlet UILabel *Screen;
+    
+}
+-(IBAction)Number0:(id)sender;
+-(IBAction)Number1:(id)sender;
+-(IBAction)Number2:(id)sender;
+-(IBAction)Number3:(id)sender;
+-(IBAction)Number4:(id)sender;
+-(IBAction)Number5:(id)sender;
+-(IBAction)Number6:(id)sender;
+-(IBAction)Number7:(id)sender;
+-(IBAction)Number8:(id)sender;
+-(IBAction)Number9:(id)sender;
+-(IBAction)Times:(id)sender;
+-(IBAction)Divide:(id)sender;
+-(IBAction)Subtract:(id)sender;
+-(IBAction)Plus:(id)sender;
+-(IBAction)Equals:(id)sender;
+-(IBAction)AllClear:(id)sender;
 @end
